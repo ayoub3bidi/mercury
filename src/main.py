cdc9fa4b7535587619c09374a7cf9b933a73d215
Ayoub Abidi <mrayoubabidi@gmail.com>
2023-10-05 17:25:05 +0100

    🩹 fix: fix swagger configuration

--- src/main.py
+++ src/main.py
@@ -4,27 +4,23 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
 import redis
-from fastapi.openapi.utils import get_openapi
 from database.postgres_db import dbEngine, Base
 import database.redis_db as redis
 
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(
-        title="Mercury API Docs",
-        version=os.environ['VERSION'],
-        description="This is the Swagger documentation of the Mercury API",
-        routes=app.routes,
-    )
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
+app_version = os.environ['APP_VERSION']
+app_title = os.environ['APP_TITLE']
+app_description = os.environ['APP_DESCRIPTION']
+v = os.environ['API_VERSION']
 
 Base.metadata.create_all(bind=dbEngine)
 redis.init()
 
-app = FastAPI(docs_url="/")
-app.openapi = custom_openapi
+app = FastAPI(
+        docs_url="/",
+        title=app_title,
+        version=app_version,
+        description=app_description,
+    )
 
 if os.getenv('APP_ENV') == 'local':
     app.add_middleware(
@@ -35,6 +31,6 @@ if os.getenv('APP_ENV') == 'local':
         allow_headers=["*"],
     )
 
-app.include_router(health.router, tags=['Information'], prefix='/v1')
-app.include_router(auth_guard.router, tags=['Access Token'], prefix='/v1')
-app.include_router(user.router, tags=['User'], prefix='/v1/user')
+app.include_router(health.router, tags=['Information'], prefix=f'/{v}')
+app.include_router(auth_guard.router, tags=['Access Token'], prefix=f'/{v}')
+app.include_router(user.router, tags=['User'], prefix=f'/{v}/user')
