98c7ff3cdf51fc7ecbbeffb6cbcb8cb3c9cc2082
Ayoub Abidi <mrayoubabidi@gmail.com>
2024-02-24 15:43:18 +0100

    fix: typo

--- ci/integration-test.sh
+++ ci/integration-test.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-echo "POSTGRES_HOST=${{ env.POSTGRES_HOST }}" > .env
-echo "POSTGRES_PORT=${{ env.POSTGRES_PORT }}" >> .env
-echo "POSTGRES_DB=${{ env.POSTGRES_DB }}" >> .env
-echo "POSTGRES_USER=${{ env.POSTGRES_USER }}" >> .env
-echo "POSTGRES_PASSWORD=${{ env.POSTGRES_PASSWORD }}" >> .env
-
 docker-compose up --build --abort-on-container-exit mercury_integration_tests
